e7f3ba22ce26e48ee02ee064e7f0ab5b33914dfb
ehsankorhani <ehsan.korhani@outlook.com>
2018-12-21 11:48:19 +1100

    poc

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 console.log('External file........................');
 
-var models = skuid.model.map(),
+document.onreadystatechange = function(){
+  if(document.readyState === 'complete'){
+    var models = skuid.model.map(),
     teamMemberViewModel = models.TeamMember_view || {},
     teamMemberViewData = teamMemberViewModel.data || [];
 
-if (teamMemberViewData.length > 0)
-  console.log('teamMemberViewData:', teamMemberViewData);
-else
-  console.log('teamMemberViewModel:', teamMemberViewModel);
+    if (teamMemberViewData.length > 0)
+      console.log('teamMemberViewData:', teamMemberViewData);
+    else
+      console.log('teamMemberViewModel:', teamMemberViewModel);
+    }
+}
